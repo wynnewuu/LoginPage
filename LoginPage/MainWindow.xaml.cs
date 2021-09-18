7f5b9fe68eb7769f073016f3c0aa10d2ed48575c
wynnewuu <wynnewuu@gmail.com>
2021-09-18 10:15:34 -0400

    upload

---
+++ LoginPage/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+
+namespace LoginPage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void okayButton_Click(object sender, RoutedEventArgs e)
+        {
+            string usernm = this.nameTextBox.Text;
+            string passwd = this.passwordTextBox.Password;
+
+            {
+                if (usernm == "" || passwd == "")
+                {
+                    MessageBox.Show("Please fill in all the slots");
+                }
+                //Making sure password contain both letter and number and minimal 6 character
+                //Make sure Password does not start with a number "char.IsDigit(Firstindex)"
+                //Password can not contain special characters
+                //Password cant not start with numeral value
+                var input = this.passwordTextBox.Password;
+                var hasChar = new Regex(@"^[a-zA-Z](?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$");
+
+
+                if (!hasChar.IsMatch(input) || ((passwd.Contains("@") || passwd.Contains("#")
+                            || passwd.Contains("!") || passwd.Contains("~")
+                            || passwd.Contains("$") || passwd.Contains("%")
+                            || passwd.Contains("^") || passwd.Contains("&")
+                            || passwd.Contains("*") || passwd.Contains("(")
+                            || passwd.Contains(")") || passwd.Contains("-")
+                            || passwd.Contains("+") || passwd.Contains("/")
+                            || passwd.Contains(":") || passwd.Contains(".")
+                            || passwd.Contains(", ") || passwd.Contains("<")
+                            || passwd.Contains(">") || passwd.Contains("?")
+                            || passwd.Contains("|"))))
+                {
+                    MessageBox.Show("A valid password needs to have at least six characters with both letters and numbers.");
+                }
+                else
+                {
+                    if (usernm != "" && passwd != "")
+                        MessageBox.Show("Thank you for providing the input, which will be verified.");
+                }
+            }
+        }
+
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- README.md
+++
@@ -1,6 +0,0 @@
-# LoginPage
-This is a simple WPF App coding using C# and Visual Studio Code that allow user to type in the username and password                       
-If the user type in the username and password the program output: Thank you for providing the input, which will be verified.            
-If user didn't type in both username and password then the program output: Please fill in all the slots            
-Here is a YouTube video demonstrating it!                      
-https://youtu.be/xMwrvhz47_U
